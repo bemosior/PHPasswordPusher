fc46a6dd00fbe3723075747ef43fcaf99893ab03
Benjamin Mosior <bemosior@ship.edu>
2012-08-28 23:07:31 -0400

    Updated MySQL installation script to properly pull from config.php. Added config.php.

--- mysqlinstall.php
+++ mysqlinstall.php
@@ -1,4 +1,20 @@
-<?php//SETUP:
-$dbname = 'phpwpush';  //The desired MySQL database name.$dbuser = 'root';  //Privileged user to perform database creation and privilege granting.$dbpass = 'rootpw';  //Privileged user's password (please remove this value when finished)//Create Databasetry{$db = new PDO("mysql:dbname=;host=localhost", $dbuser, $dbpass) or die('Connect Failed');$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);$db->exec('CREATE DATABASE ' . $dbname . ';'); $db->exec('USE ' . $dbname .';');$db->exec('CREATE TABLE phpasspush (seccred VARCHAR(128), id VARCHAR(128) NOT NULL PRIMARY KEY, ctime DATETIME, xtime DATETIME, views INT, xviews INT);') ;$db->exec('CREATE EVENT phpasspush_tidy ON SCHEDULE EVERY 5 MINUTE DO UPDATE ' . $dbname . '.phpasspush SET seccred=NULL WHERE xtime<UTC_TIMESTAMP() OR views>=xviews;') ;$db->exec('GRANT ALL PRIVILEGES ON ' . $dbname . '.* TO \'' . $dbuser . '\'@\'localhost\' IDENTIFIED BY \'' . $dbpass . '\';') ;
-} catch (PDOException $e) {  echo 'Problem: ' . $e->getMessage() . "\n";}
+<?php
+//SETUP:
+$rootdbuser = 'root';  //Privileged user to perform database creation and privilege granting.
+$rootdbpass = 'rootpw';  //Privileged user's password (please remove this value when finished)
+
+require 'config.php';
+
+//Create Database
+try{
+  $db = new PDO("mysql:dbname=;host=localhost", $rootdbuser, $rootdbpass) or die('Connect Failed');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->exec('CREATE DATABASE ' . $dbname . ';'); 
+  $db->exec('USE ' . $dbname .';');
+  $db->exec('CREATE TABLE phpasspush (seccred VARCHAR(128), id VARCHAR(128) NOT NULL PRIMARY KEY, ctime DATETIME, xtime DATETIME, views INT, xviews INT);') ;
+  $db->exec('CREATE EVENT phpasspush_tidy ON SCHEDULE EVERY 5 MINUTE DO UPDATE ' . $dbname . '.phpasspush SET seccred=NULL WHERE xtime<UTC_TIMESTAMP() OR views>=xviews;');
+  $db->exec('GRANT ALL PRIVILEGES ON ' . $dbname . '.* TO \'' . $dbuser . '\'@\'localhost\' IDENTIFIED BY \'' . $dbpass . '\';') ;
+} catch (PDOException $e) {
+  echo 'Problem: ' . $e->getMessage() . "\n";
+}
 ?>
