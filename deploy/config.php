b485c32ef821293fdc7e340e729f563b112a34e1
Benjamin Mosior <bemosior@ship.edu>
2012-09-09 23:10:43 -0400

    Reorganized configuration file.

--- deploy/config.php
+++ deploy/config.php
@@ -1,16 +1,22 @@
 <?php
-//SETUP:
+//MySQL and Apache configuration:
 $dbname = 'phpwpush'; //The desired MySQL database name.
 $dbuser = 'phpw';  //The credential lookup user
 $dbpass = 'phpass!3#.';  //The credential lookup user's password
 $installation = '/phpw';  //The installation location (path to where PHPasswordPusher is hosted)
-$key = "change this key"; // Just change this to something random.
-$xtime_default = 15; // default minutes until expires
-$xviews_default = 2; // default # of views before it expires
-$logoname = "phpwpusher.png";
+
+//Encryption and default limits:
+$key = "change this key"; //This is the encryption key. Change it to something random.
+$xtime_default = 15; //default minutes until the link expires
+$xviews_default = 2; //default # of views before the link expires
 $submitwarning = 'Submitted credentials will expire and be erased according to the time or view limit, '
   . 'whichever occurs first. NEVER leave credentials where they can be easily accessed by others.';
 $retrievewarning = 'NEVER leave credentials where they can be easily accessed by others. '
   . '<!--We recommend using KeePass (<a href=\"http://keepass.info/\">http://keepass.info</a>).-->';
+
+//User interface misc.:
+$logoname = "phpwpusher.png"; // 
 $title='Secure Password Sharing Utility';
+
+
 ?>
