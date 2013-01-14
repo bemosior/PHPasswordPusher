d41a98556c9787a08eabd7a16de970dff5fbaaae
Benjamin Mosior <bemosior@ship.edu>
2013-01-14 06:13:54 -0500

    Added success confirmation to install script.

--- install.php
+++ install.php
@@ -20,6 +20,7 @@ try{
   $db->exec('CREATE TABLE phpasspush (seccred TEXT, id VARCHAR(128) NOT NULL PRIMARY KEY, ctime DATETIME, xtime DATETIME, views INT, xviews INT);') ;
   $db->exec('CREATE EVENT phpasspush_tidy ON SCHEDULE EVERY 5 MINUTE DO UPDATE ' . $dbname . '.phpasspush SET seccred=NULL WHERE xtime<UTC_TIMESTAMP() OR views>=xviews;');
   $db->exec('GRANT ALL PRIVILEGES ON ' . $dbname . '.* TO \'' . $dbuser . '\'@\'localhost\' IDENTIFIED BY \'' . $dbpass . '\';') ;
+  echo "MySQL setup is successful!\n";
 } catch (PDOException $e) {
   echo 'Problem: ' . $e->getMessage() . "\n";
 }
