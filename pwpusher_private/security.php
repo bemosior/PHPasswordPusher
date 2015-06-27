73eaa10454594ea3b61f1c8773ecf41441ae09a5
Benjamin Mosior <bemosior@gmail.com>
2015-06-27 12:05:56 -0400

    Refs #31 - AES encryption now in use.

--- pwpusher_private/security.php
+++ pwpusher_private/security.php
@@ -17,10 +17,10 @@ function encryptCred($cred)
     include 'config.php';
     $encrypted = base64_encode(
         mcrypt_encrypt(
-            MCRYPT_RIJNDAEL_256, $key, $cred, MCRYPT_MODE_ECB,
+            MCRYPT_RIJNDAEL_128, $key, $cred, MCRYPT_MODE_ECB,
             mcrypt_create_iv(
                 mcrypt_get_iv_size(
-                    MCRYPT_RIJNDAEL_256, 
+                    MCRYPT_RIJNDAEL_128,
                     MCRYPT_MODE_ECB
                 ), 
                 MCRYPT_RAND
@@ -41,13 +41,13 @@ function decryptCred($encrypted)
 {
     include 'config.php';
     $decrypted = mcrypt_decrypt(
-        MCRYPT_RIJNDAEL_256,
+        MCRYPT_RIJNDAEL_128,
         $key,
         base64_decode($encrypted),
         MCRYPT_MODE_ECB,
         mcrypt_create_iv(
             mcrypt_get_iv_size(
-                MCRYPT_RIJNDAEL_256, 
+                MCRYPT_RIJNDAEL_128,
                 MCRYPT_MODE_ECB
             ), 
             MCRYPT_RAND
